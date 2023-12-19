62a4e9d66ce6c4dfc3050bf19841e9940f358b76
Ryan Gabriel <ryan.bromati@hotmail.com>
2023-12-19 16:58:30 -0300

    feat: todoitem entity and todochoice enum created

---
+++ src/InternTaskTracker.Core/Domain/TodoItem.cs
@@ -0,0 +1,10 @@
+namespace InternTaskTracker.Core.Domain;
+
+public class TodoItem(string description, bool isComplete = false)
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Description { get; set; } = description;
+    public bool IsComplete { get; set; } = isComplete;
+}
+ 
+public enum TodoChoice{Unknown, Add, Remove, MarkAsCompleted, View, Exit}
